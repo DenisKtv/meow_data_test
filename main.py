033efd0e9b7715ad18467e65643a3b7b178a1594
Denis Kostiv <deniskostiv@gmail.com>
2023-09-05 14:08:29 +0300

    add func create_table

---
+++ main.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+DATABASE_NAME = 'cheaters.db'
+
+
+def create_table():
+    """Создает таблицу с заданными полями в базе данных."""
+    try:
+        connection = sqlite3.connect(DATABASE_NAME)
+        cursor = connection.cursor()
+
+        create_table_query = '''CREATE TABLE IF NOT EXISTS data_table
+                                (timestamp TEXT,
+                                player_id INTEGER,
+                                event_id INTEGER,
+                                error_id INTEGER,
+                                json_server TEXT,
+                                json_client TEXT);'''
+
+        cursor.execute(create_table_query)
+        connection.commit()
+
+    except sqlite3.Error as e:
+        print(f'SQLite error: {e}')
+
+    finally:
+        if connection:
+            connection.close()
+
+
+if __name__ == '__main__':
+    create_table()
